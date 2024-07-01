e8d8fb7c99df45419d82d1c23bb7c422708cd0b6
Михаил А. Борисенко <BMA@start-npf.yaroslavl.local>
2024-07-01 11:19:16 +0300

    bma 6 func is good close testmain

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ func main() {
 }
 
 func MaxInt(a, b int) int {
-	if a < b { // чтоб сломать тест
-		// if a >= b { // это верно
+	//	if a < b { // чтоб сломать тест
+	if a >= b { // это верно
 		return a
 	}
 
